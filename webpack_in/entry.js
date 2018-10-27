0d7e6dcaa9052919f74c9da2fda41606f9ba1e06
Marat Nepomnyashy <maratbn@gmail.com>
2018-10-27 16:35:55 -0700

    Changed the boilerplate content from example template to the Emolingo Emotion Synonym Match Game Web App.

--- webpack_in/entry.js
+++ webpack_in/entry.js
@@ -1,6 +1,1 @@
-//  This file 'entry.jsx' is part of an example for building a multi-widget React front-end app
-//  step by step as outlined in the tutorial blog at
-//  http://maratbn.com/blogs/2018/07/02/react-multi-widget/
-
-
 console.log("JavaScript entry logic.");
